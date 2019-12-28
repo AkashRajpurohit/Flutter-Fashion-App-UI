150604f6c4113dbb88debdfa5bd29ee4fddbc37c
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-12-28 18:13:43 +0530

    :art: card container

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -34,6 +34,7 @@ class _HomeState extends State<Home> {
         children: <Widget>[
           Container(
             height: 150.0,
+            width: double.infinity,
             child: ListView(
               scrollDirection: Axis.horizontal,
               padding: EdgeInsets.all(10.0),
@@ -46,7 +47,23 @@ class _HomeState extends State<Home> {
                 _listItem("assets/model-6.jpeg", "assets/brand-6.jpeg"),
               ],
             ),
-          )
+          ),
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(15.0),
+              elevation: 4.0,
+              child: Container(
+                height: 450.0,
+                width: double.infinity,
+                padding: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
