efe5eb553b08d6d25c95288a1882d6e830dee81c
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-12-28 17:53:32 +0530

    :art: homepage horizontal scrollable list view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
         title: Text(
           'Discovery',
           style: TextStyle(
-            fontSize: 20.0,
+            fontSize: 25.0,
             fontWeight: FontWeight.bold,
             color: Colors.black
           ),
@@ -25,6 +25,85 @@ class _HomeState extends State<Home> {
             icon: Icon(Icons.photo_camera),
             color: Colors.grey,
             onPressed: () {},
+            iconSize: 30.0,
+          )
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 15.0),
+        children: <Widget>[
+          Container(
+            height: 150.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.all(10.0),
+              children: <Widget>[
+                _listItem("https://s3.amazonaws.com/uifaces/faces/twitter/marklamb/128.jpg", "http://lorempixel.com/640/480"),
+                _listItem("https://s3.amazonaws.com/uifaces/faces/twitter/dhooyenga/128.jpg", "http://lorempixel.com/640/480/business"),
+                _listItem("https://s3.amazonaws.com/uifaces/faces/twitter/skkirilov/128.jpg", "http://lorempixel.com/640/480/fashion"),
+                _listItem("https://s3.amazonaws.com/uifaces/faces/twitter/joreira/128.jpg", "http://lorempixel.com/640/480/city"),
+                _listItem("https://s3.amazonaws.com/uifaces/faces/twitter/sethlouey/128.jpg", "http://lorempixel.com/640/480/cats"),
+                _listItem("https://s3.amazonaws.com/uifaces/faces/twitter/russell_baylis/128.jpg", "http://lorempixel.com/640/480/technics")
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _listItem(String imgPath, String logo) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 75.0,
+                width: 75.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(37.5),
+                  image: DecorationImage(
+                    image: NetworkImage(imgPath),
+                    fit: BoxFit.cover
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 50.0,
+                left: 50.0,
+                child: Container(
+                  height: 25.0,
+                  width: 25.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.5),
+                    image: DecorationImage(
+                      image: NetworkImage(logo),
+                      fit: BoxFit.cover
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            height: 30.0,
+            width: 75.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: Colors.brown,
+            ),
+            child: Center(
+              child: Text(
+                "Follow",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0
+                ),
+              ),
+            ),
           )
         ],
       ),
